49fbfe4b4719aa250a857f99bf1fea6d2bd3352f
Ryan <ryannjueleo0987@gmail.com>
2025-07-10 00:53:25 +0300

    chore: wrap app with AuthProvider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BookingProvider } from './BookingContext'; // ⬅ import
+import { BookingProvider } from './BookingContext';
+import { AuthProvider } from 'context/AuthContext';   // ✅ NEW
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BookingProvider>  {/* ⬅ wrap App here */}
-      <App />
-    </BookingProvider>
+-   <BookingProvider>
+-     <App />
+-   </BookingProvider>
++   <AuthProvider>            {/* 🔐 makes auth available app-wide */}
++     <BookingProvider>       {/* 🧹 your existing context */}
++       <App />
++     </BookingProvider>
++   </AuthProvider>
   </React.StrictMode>
 );
